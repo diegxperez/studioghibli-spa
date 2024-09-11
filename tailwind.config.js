e35c5a1183aaaf4d81157cd4ef781d5fc64cc965
diegxperez <diegouceda9@gmail.com>
2024-09-11 18:23:52 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Ajusta la ruta según tu estructura de proyecto
+  theme: {
+    extend: {
+      fontFamily: {
+        dotGothic: ["DotGothic16, sans-serif"],
+        geistMono: ["Geist Mono, monospace"],
+      },
+      colors: {
+        brandprimary: "#DDF9EF",
+        brandsecondary: "#FBF8EB",
+        graysoft: "#575757",
+      },
+    },
+  },
+  plugins: [],
+};
