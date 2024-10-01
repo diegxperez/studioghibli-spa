4c55280bec870a73acd4c277a58eaeaeba5a27d7
diegxperez <diegouceda9@gmail.com>
2024-10-01 16:02:55 -0500

    Fix: function toogleBtnFavorite v2

--- src/assets/views/movieDetails.js
+++ src/assets/views/movieDetails.js
@@ -69,23 +69,24 @@ function saveMovie(movie) {
   const starBtnMovie = $('#button-favorite-star');
   let movies = localStorage.getItem('movies') ? JSON.parse(localStorage.getItem('movies')) : [];
   let indexMovie = movies.findIndex((movie_localStorage) => movie_localStorage.id == movie.id);
-  if (indexMovie >= 0) {
-    toogleBtnFavorite(btnMovie, starBtnMovie);
+  if (indexMovie == 0 || indexMovie > 0) {
+    verifyBtnFavorite(btnMovie, starBtnMovie);
   }
 
   btnMovie.addEventListener('click', function () {
     let movies = localStorage.getItem('movies') ? JSON.parse(localStorage.getItem('movies')) : [];
     let indexMovie = movies.findIndex((movie_localStorage) => movie_localStorage.id == movie.id);
-
-    if (indexMovie >= 0) {
+    if (indexMovie == 0 || indexMovie > 0) {
       movies.splice(indexMovie, 1)
+      localStorage.setItem('movies', JSON.stringify(movies));
       toogleBtnFavorite(btnMovie, starBtnMovie);
+
     } else {
       movies.push(movie);
+      localStorage.setItem('movies', JSON.stringify(movies));
       toogleBtnFavorite(btnMovie, starBtnMovie);
-    }
 
-    localStorage.setItem('movies', JSON.stringify(movies));
+    }
   })
 }
 
